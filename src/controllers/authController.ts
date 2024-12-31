5f9475c9eb83c06d5c1b0aae98384b1bc2d9e719
omervavd3 <omervavd3@gmail.com>
2024-12-31 17:50:58 +0200

    jwt start

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,26 +1,60 @@
 import UserModel from "../models/userModel";
 import { Request, Response } from "express";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const register = async (req: Request, res: Response) => {
-    const {email, password} = req.body;
-    if(email == null || password == null) {
-        return res.status(400).send("Email and password are required");
-    }
     try {
+        const {email, password} = req.body;
+        if(email == null || password == null) {
+            res.status(400).send("Email and password are required");
+            return;
+        }
         const user = await UserModel.findOne({email});
         if(user) {
-            return res.status(400).send("User already exists");
+            res.status(400).send("User already exists");
+            return;
         }
-        await UserModel.create({email, password});
-        res.status(200).send("User created");
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+        const newUser = await UserModel.create({email, hashedPassword});
+        res.status(200).send(newUser);
+        return;
     } catch (error) {
         res.status(500).send(error);
     }
 };
 
 const login = async (req: Request, res: Response) => {
-    console.log("login");
-    res.status(200).send("login");
+    try {
+        const {email, password} = req.body;
+        if(email == null || password == null) {
+            res.status(400).send("Email and password are required");
+            return;
+        }
+        const user = await UserModel.findOne({email});
+        if(user == null) {
+            res.status(400).send("User does not exist");
+            return;
+        }
+        const valid = await bcrypt.compare(password, user.password);
+        if(!valid) {
+            res.status(400).send("Invalid password");
+            return;
+        }
+        if(process.env.ACCESS_TOKEN_SECRET == null || process.env.JWT_EXPIRES_IN == null) {
+            res.status(500).send("Internal server error");
+            return;
+        }
+        const accesToken = await jwt.sign(
+            {'_id': user._id}, 
+            process.env.ACCESS_TOKEN_SECRET,
+            {expiresIn: process.env.JWT_EXPIRES_IN}
+        );
+        res.status(200).send(accesToken);
+    } catch (error) {
+        res.status(500).send(error);
+    }
 };
 
 const logout = async (req: Request, res: Response) => {
@@ -28,8 +62,36 @@ const logout = async (req: Request, res: Response) => {
     res.status(200).send("logout");
 }
 
+type payload = {
+    _id: string
+}
+
+const autMiddleware = async (req: Request, res: Response, next: any) => {
+    const authHeaders = req.headers['authorization'];
+    const token = authHeaders && authHeaders.split(' ')[1];
+    if(token == null) {
+        res.status(401).send("Unauthorized");
+        return;
+    }
+
+    if(process.env.ACCESS_TOKEN_SECRET == null) {
+        res.status(500).send("Internal server error");
+        return;
+    }
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, payload) => {
+        if(err) {
+            res.status(403).send("Forbidden");
+            return;
+        }
+        req.params.userId = (payload as payload)._id;
+        next();
+    })
+}
+
 export default {
     register,
     login,
-    logout
+    logout,
+    autMiddleware
 };
--- src/routes/postRoutes.ts
+++ src/routes/postRoutes.ts
@@ -1,11 +1,12 @@
 import express from "express";
 const postRouter = express.Router();
 import PostController from "../controllers/postController";
+import authController from "../controllers/authController";
 
 postRouter
     .get("/", PostController.getAll.bind(PostController))
-    .post("/", PostController.create.bind(PostController))
+    .post("/", authController.autMiddleware ,PostController.create.bind(PostController))
     .get("/:id", PostController.getById.bind(PostController))
-    .delete("/:id", PostController.deleteItemById.bind(PostController))
+    .delete("/:id", authController.autMiddleware ,PostController.deleteItemById.bind(PostController))
 
 export default postRouter;
